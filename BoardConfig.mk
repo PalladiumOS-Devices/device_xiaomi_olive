884939c6f9fd4a3416b0d5baa0ffac1fed0cec5d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-05 19:29:18 +0200

    olive: Inherit from common tree
    Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

---
+++ BoardConfig.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common sdm439-common
+include device/xiaomi/sdm439-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/olive
+
+# Partitions
+BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4831838208
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1610612736
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-03-01
+
+# Inherit from the proprietary version
+include vendor/xiaomi/olive/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,1 @@
+# Device Tree for Xiaomi Redmi 8 (olive)
