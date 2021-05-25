99e70d5e9d35cb8cd851ff66df329b8060b13dc3
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-25 10:01:29 +0800

    olives: prop: Import stock camera props

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953.xml
 
+# Properties
+include $(LOCAL_PATH)/properties.mk
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
---
+++ properties.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.camera.aec.sync=1 \
+persist.vendor.camera.awb.sync=2 \
+persist.vendor.camera.expose.aux=1
