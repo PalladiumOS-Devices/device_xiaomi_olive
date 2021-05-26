f3d2adc82f16812ea96b993d6ff716a9c1a39045
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-26 18:50:05 +0800

    olive: rootdir: Import camera init.rc instructions from stock Q
    Xiaomi/olive/olive:10/QKQ1.191014.001/V12.0.2.0.QCNMIXM:user/release-keys

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ PRODUCT_PACKAGES += \
 # Properties
 include $(LOCAL_PATH)/properties.mk
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.olive.rc
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
