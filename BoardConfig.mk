714dd32b202714c3fffd76a653aea34911999f0d
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-31 20:39:24 +0800

    olives: sepolicy: Address IR Denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,7 @@ VENDOR_SECURITY_PATCH := 2021-03-01
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/biometrics/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Inherit from the proprietary version
 include vendor/xiaomi/olive/BoardConfigVendor.mk
