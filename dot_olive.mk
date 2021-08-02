e420ad03443f2723ed53a42ee3a865f47c2a4987
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-08-02 07:30:20 +0000

    olive: Sync vendor SPL and system fingerprint with MIUI V12.0.3.0.QCNMIXM

--- dot_olive.mk
+++ dot_olive.mk
@@ -27,4 +27,4 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.0.2.0.QCNMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.0.3.0.QCNMIXM:user/release-keys"
