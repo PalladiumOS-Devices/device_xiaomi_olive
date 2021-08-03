82a9943f305cebabf13255bb7f7a0756fb97d2c9
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-08-03 18:13:32 +0000

    olive: Import vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# Track our display hals
+rm -rf hardware/qcom-caf/msm8996-los/display
+git clone https://github.com/mi-sdm439/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996-los/display
