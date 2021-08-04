993e82d7913d9971dadc8764c1205296a2e9052f
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-08-04 21:15:25 +0000

    olive: overlay-lineage: Rebrand to dot

--- device.mk
+++ device.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/xiaomi/sdm439-common/sdm439.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-dot
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1572
